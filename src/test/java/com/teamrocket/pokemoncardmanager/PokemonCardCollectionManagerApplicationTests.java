4e1b364963406846c57dd335aa22d777089d9594
AdamBadagliacco <48744499+AdamBadagliacco@users.noreply.github.com>
2021-01-08 13:52:35 -0600

    Initial Upload

---
+++ src/test/java/com/teamrocket/pokemoncardmanager/PokemonCardCollectionManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamrocket.pokemoncardmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonCardCollectionManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
