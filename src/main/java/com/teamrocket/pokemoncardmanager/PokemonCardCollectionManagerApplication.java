2e241547a527c369aa85455fea3f70eeb540ad2e
AdamBadagliacco <48744499+AdamBadagliacco@users.noreply.github.com>
2021-01-11 13:18:41 -0600

    Added The code for tokens
    Added the code from this article:
    
    https://blog.softtek.com/en/token-based-api-authentication-with-spring-and-jwt

--- src/main/java/com/teamrocket/pokemoncardmanager/PokemonCardCollectionManagerApplication.java
+++ src/main/java/com/teamrocket/pokemoncardmanager/PokemonCardCollectionManagerApplication.java
@@ -2,12 +2,37 @@ package com.teamrocket.pokemoncardmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.teamrocket.pokemoncardmanager.security.JWTAuthorizationFilter;
 
 @SpringBootApplication
 public class PokemonCardCollectionManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PokemonCardCollectionManagerApplication.class, args);
+	}
+	
+	
+	//Below is where we determine which pages need authorization to access
+	
+	@EnableWebSecurity
+	@Configuration
+	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.csrf().disable()
+				.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+				.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/user").permitAll()
+				.anyRequest().authenticated();
+		}
 	}
 
 }
--- src/main/java/com/teamrocket/pokemoncardmanager/entities/User.java
+++ src/main/java/com/teamrocket/pokemoncardmanager/entities/User.java
@@ -4,6 +4,7 @@ public class User {
 	private int id;
 	private String name;
 	private String password;
+	private String token;
 	public int getId() {
 		return id;
 	}
@@ -21,6 +22,10 @@ public class User {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	public void setToken(String token) {
+		this.token = token;
+		
 	}
 	
 	
